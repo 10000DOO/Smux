ba1e86c6939d84d695b3c7a46a30c8ee69a68d36
10000DOO <gunjoun99@gmail.com>
2026-04-24 18:32:02 +0900

    Initial Commit

---
+++ Smux/SmuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmuxApp.swift
+//  Smux
+//
+//  Created by 이건준 on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct SmuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
